169ac86a1d77428c3e2126cd772686337f4e38ed
jackcornblum <jcornblum@gmail.com>
2021-04-25 15:02:54 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,73 @@
+let number = 5;
+
+function add(number) {
+    return number += 5;
+}
+
+add();
+
+function subtract(number) {
+    return number -= 5;
+}
+
+subtract();
+
+function multiply(number) {
+    return number *= 5;
+}
+
+multiply();
+
+function divide(number) {
+    return number /= 5;
+}
+
+divide();
+
+function add(a, b) {
+    return a + b;
+}
+
+add(959,2);
+
+function subtract(a,b) {
+    return a - b;
+}
+
+subtract(959,2);
+
+function multiply(a, b) {
+    return a * b;
+}
+
+multiply(959,2);
+
+function divide(a,b) {
+    return a / b;
+}
+
+multiply(959,2);
+
+function increment(n) {
+    return ++n;
+}
+
+increment(823);
+
+function decrement(n) {
+    return --n;
+}
+
+decrement(723);
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+makeInt(72);
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+preserveDecimal(73.432);
